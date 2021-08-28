448cd4cd14484cf8315139fddd6cd81c9e41c3c6
Billy <72440832+billybot482@users.noreply.github.com>
2021-08-28 22:57:08 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,2 +1,12 @@
 updater = Updater(token='1736686159:AAFG0jC4qEHE5ahhc_F7kZY-LMH5UR1lxAM', use_context=True)
 dispatcher = updater.dispatcher
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger()
+
+updater.start_polling(drop_pending_updates = True)
+
+
