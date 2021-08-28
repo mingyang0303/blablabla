b3ea0093734543a9e3d588888ed6588a6c0a6f29
Billy <72440832+billybot482@users.noreply.github.com>
2021-08-28 22:57:48 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,10 @@
+import logging
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import CommandHandler, InlineQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext , Filters
+import random
+
 updater = Updater(token='1736686159:AAFG0jC4qEHE5ahhc_F7kZY-LMH5UR1lxAM', use_context=True)
 dispatcher = updater.dispatcher
 
