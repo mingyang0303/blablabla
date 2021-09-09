7aa4dd3a7e15e80e5eeff84408b5da348b53a9a9
Billy <72440832+billybot482@users.noreply.github.com>
2021-09-09 17:51:35 +0800

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1254,7 +1254,7 @@ def sudo_list(update, context):
 def bet(update, context):
     id = update.effective_user.id
     name = update.effective_user.first_name
-    from_gold = DB.get_user_value(from_id, "diamonds")
+    from_gold = DB.get_user_value(id, "diamonds")
     try:
      msg = update.message.text.split(None,1)[1]
      msg = int(msg)
