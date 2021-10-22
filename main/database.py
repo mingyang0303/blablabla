617f59c1e56addfdcd3fc2d7d1852a4cdbc2c2dd
Billy <72440832+billybot482@users.noreply.github.com>
2021-10-22 15:47:37 +0800

    Update database.py

--- main/database.py
+++ main/database.py
@@ -65,7 +65,6 @@ def add_column():
 
 def get_name(user_id: int, items: str):
     stmt = f"SELECT name FROM Usr WHERE user_id=%s;"
-    try:
      try:
      cur.execute(stmt)
      r = cur.fetchall()
